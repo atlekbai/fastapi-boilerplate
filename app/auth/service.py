d3b789f2cf0cd7191c35ee185a01a8d5e5965280
Alimukhamed Tlekbai <ali.tlekbai@gmail.com>
2023-06-14 11:38:13 +0600

    add: .env

--- app/auth/service.py
+++ app/auth/service.py
@@ -1,33 +1,11 @@
-from pydantic import BaseSettings
-
 from app.config import database
-
-from .adapters.jwt_service import JwtService
-from .repository.repository import AuthRepository
-
-
-class AuthConfig(BaseSettings):
-    JWT_ALG: str = "HS256"
-    JWT_SECRET: str = "YOUR_SUPER_SECRET_STRING"
-    JWT_EXP: int = 10_800
-
-
-config = AuthConfig()
 
 
 class Service:
-    def __init__(
-        self,
-        repository: AuthRepository,
-        jwt_svc: JwtService,
-    ):
-        self.repository = repository
-        self.jwt_svc = jwt_svc
+    def __init__(self):
+        pass
 
 
 def get_service():
-    repository = AuthRepository(database)
-    jwt_svc = JwtService(config.JWT_ALG, config.JWT_SECRET, config.JWT_EXP)
-
-    svc = Service(repository, jwt_svc)
+    svc = Service()
     return svc
